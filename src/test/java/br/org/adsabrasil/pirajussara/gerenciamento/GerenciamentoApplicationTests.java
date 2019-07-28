abdfdfa3c85589f2a0544d85d8eaeee97a09a520
Michael Barroso <michael_p._b.15600@hotmail.com>
2019-07-28 08:08:23 -0300

    Initial commit

---
+++ src/test/java/br/org/adsabrasil/pirajussara/gerenciamento/GerenciamentoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.org.adsabrasil.pirajussara.gerenciamento;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GerenciamentoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
